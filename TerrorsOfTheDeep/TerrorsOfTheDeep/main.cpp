b722211c96ab3f052761d2e1bce5104838f0d799
Simone Bakker <simonebakker94@gmail.com>
2018-03-08 13:17:02 +0100

    Player as child of camera
    The player node is now a child of the camera and moves with it

--- TerrorsOfTheDeep/TerrorsOfTheDeep/main.cpp
+++ TerrorsOfTheDeep/TerrorsOfTheDeep/main.cpp
@@ -13,44 +13,10 @@ using namespace gui;
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
-
-class MyEventReceiver : public IEventReceiver
-{
-public:
-	// This is the one method that we have to implement
-	virtual bool OnEvent(const SEvent& event)
-	{
-		// Remember whether each key is down or up
-		if (event.EventType == irr::EET_KEY_INPUT_EVENT)
-			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
-
-		return false;
-	}
-
-	// This is used to check whether a key is being held down
-	virtual bool IsKeyDown(EKEY_CODE keyCode) const
-	{
-		return KeyIsDown[keyCode];
-	}
-
-	MyEventReceiver()
-	{
-		for (u32 i = 0; i<KEY_KEY_CODES_COUNT; ++i)
-			KeyIsDown[i] = false;
-	}
-
-private:
-	// We use this array to store the current state of each key
-	bool KeyIsDown[KEY_KEY_CODES_COUNT];
-};
-
 int main()
 {
-	MyEventReceiver receiver;
-
-	IrrlichtDevice *device =
-		createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
-			false, false, false, &receiver);
+	IrrlichtDevice *device = createDevice(EDT_OPENGL, dimension2d<u32>(640, 480), 16,
+			false, false, false, 0);
 
 	if (!device)
 		return 1;
@@ -59,7 +25,7 @@ int main()
 	ISceneManager* smgr = device->getSceneManager();
 	IGUIEnvironment* guienv = device->getGUIEnvironment();
 
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
+	guienv->addStaticText(L"Hi kids! This is what your parents don't want you to see!",
 		rect<s32>(10,10,260,22), true);
 
 	IAnimatedMesh* mesh = smgr->getMesh("../media/sydney.md2");
@@ -68,16 +34,17 @@ int main()
 		device->drop();
 		return 1;
 	}
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
 
+	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(mesh);
 	if (node)
 	{
+		node->setPosition(vector3df(0, 0, 60));
 		node->setMaterialFlag(EMF_LIGHTING, false);
-		node->setMD2Animation(scene::EMAT_STAND);
+		node->setMD2Animation(EMAT_STAND);
 		node->setMaterialTexture( 0, driver->getTexture("../media/sydney.bmp") );
 	}
 
-	float zCameraSpeed = 1;
+	float zCameraSpeed = 0.1;
 	float rotationCameraSpeed = 100;
 	SKeyMap keyMap[8];
 
@@ -94,6 +61,15 @@ int main()
 	keyMap[4].KeyCode = KEY_KEY_D;
 
 	smgr->addCameraSceneNodeFPS(0, rotationCameraSpeed, zCameraSpeed, -100, keyMap, 8); //(?, rotation speed, forward speed, ? , keymap, array keys keymap
+	
+	ISceneNode * player = smgr->addCubeSceneNode();
+	if (player)
+	{
+		player->setMaterialTexture(0, driver->getTexture("../../media/t351sml.jpg"));
+		player->setMaterialFlag(EMF_LIGHTING, false);
+		smgr->getActiveCamera()->addChild(player);
+	}
+	
 	device->getCursorControl()->setVisible(false);
 
 	int lastFPS = -1;
@@ -107,38 +83,6 @@ int main()
 
 	while(device->run())
 	{
-		// Work out a frame delta time.
-		const u32 now = device->getTimer()->getTime();
-		const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
-		then = now;
-
-		vector3df nodePosition = node->getPosition();
-
-		// gets direction camera is pointed in
-		ICameraSceneNode *pCamera = smgr->getActiveCamera();
-		vector3df pos = pCamera->getAbsolutePosition();
-		vector3df target = pCamera->getTarget();
-		vector3df dir = target - pos;
-
-		vector3df rot = pCamera->getRotation();
-
-		if (receiver.IsKeyDown(irr::KEY_UP))
-			nodePosition += 5 * dir * frameDeltaTime;
-		else if (receiver.IsKeyDown(irr::KEY_DOWN))
-			nodePosition -= 5 * dir * frameDeltaTime;
-
-		// for left and right directions
-		f32 roty_rad = rot.Y;
-		roty_rad -= 90;
-		roty_rad *= PI / 180;
-		vector3df rotPos = vector3df(sin(roty_rad), 0, cos(roty_rad));
-		if (receiver.IsKeyDown(irr::KEY_LEFT))
-			nodePosition += 10 * rotPos * frameDeltaTime;
-		else if (receiver.IsKeyDown(irr::KEY_RIGHT))
-			nodePosition -= 10 * rotPos * frameDeltaTime;
-
-		node->setPosition(nodePosition);
-
 		driver->beginScene(true, true, SColor(255,100,101,140));
 
 		smgr->drawAll();
@@ -150,9 +94,7 @@ int main()
 
 		if (lastFPS != fps)
 		{
-			core::stringw tmp(L"Movement Example - Irrlicht Engine [");
-			tmp += driver->getName();
-			tmp += L"] fps: ";
+			stringw tmp(L"TERRORS OF THE DEEP. fps: ");
 			tmp += fps;
 
 			device->setWindowCaption(tmp.c_str());
