181f5472a2ca9268794e6fe1cc818d6b1845d3e7
Brendan van Walstijn <brendan.van.walstijn@hva.nl>
2018-03-12 11:15:38 +0100

    Added win condition
    When the player collides with the win condition, The game displays "Congrats you win! xDDDDDD"

--- TerrorsOfTheDeep/TerrorsOfTheDeep/main.cpp
+++ TerrorsOfTheDeep/TerrorsOfTheDeep/main.cpp
@@ -88,7 +88,7 @@ IGUIFont* font;
 bool Col(ISceneNode* objectOne, ISceneNode* objectTwo, int size) {
 	if (objectOne->getAbsolutePosition().getDistanceFrom(objectTwo->getAbsolutePosition()) < size) {
 		return true;
-	}
+			}
 	else { return false; }
 }
 
@@ -171,31 +171,17 @@ int main()
 	ISceneNode* shark = smgr->addCubeSceneNode();
 	shark->setPosition(vector3df(0, 0, 400));
 
+	// adding a third cube for the win condition.
+	ISceneNode* win = smgr->addCubeSceneNode();
+	win->setPosition(vector3df(-5, 0, 600));
+	//win->setScale(vector3df(5, 5, 5));
+
 	// array of SceneNode's, Don't need this for now. 
 	//core::array<scene::ISceneNode *> objects;
-	
 
-
-	/*
-	We add a hello world label to the window, using the GUI environment.
-	The text is placed at the position (10,10) as top left corner and
-	(260,22) as lower right corner.
-	*/
 	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
 		rect<s32>(10,10,260,22), true);
 
-	/*
-	To show something interesting, we load a Quake 2 model and display it.
-	We only have to get the Mesh from the Scene Manager with getMesh() and add
-	a SceneNode to display the mesh with addAnimatedMeshSceneNode(). We
-	check the return value of getMesh() to become aware of loading problems
-	and other errors.
-
-	Instead of writing the filename sydney.md2, it would also be possible
-	to load a Maya object file (.obj), a complete Quake3 map (.bsp) or any
-	other supported file format. By the way, that cool Quake 2 model
-	called sydney was modelled by Brian Collins.
-	*/
 	IAnimatedMesh* mesh = smgr->getMesh("../media/sydney.md2");
 	if (!mesh)
 	{
@@ -204,26 +190,12 @@ int main()
 	}
 	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
 
-	/*
-	To let the mesh look a little bit nicer, we change its material. We
-	disable lighting because we do not have a dynamic light in here, and
-	the mesh would be totally black otherwise. Then we set the frame loop,
-	such that the predefined STAND animation is used. And last, we apply a
-	texture to the mesh. Without it the mesh would be drawn using only a
-	color.
-	*/
 	if (node)
 	{
 		node->setMaterialFlag(EMF_LIGHTING, false);
 		node->setMD2Animation(scene::EMAT_STAND);
 		node->setMaterialTexture( 0, driver->getTexture("../media/sydney.bmp") );
 	}
-
-	/*
-	To look at the mesh, we place a camera into 3d space at the position
-	(0, 30, -40). The camera looks from there to (0,5,0), which is
-	approximately the place where our md2 model is.
-	*/
 	//smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
 
 	//! Key map added to allow multiple keys for actions such as
@@ -247,65 +219,40 @@ int main()
 	smgr->addCameraSceneNodeFPS(0, rotationCameraSpeed, zCameraSpeed, -100, keyMap, 8); //(?, rotation speed, forward speed, ? , keymap, array keys keymap
 	device->getCursorControl()->setVisible(false);
 
-	/*
-	Ok, now we have set up the scene, lets draw everything: We run the
-	device in a while() loop, until the device does not want to run any
-	more. This would be when the user closes the window or presses ALT+F4
-	(or whatever keycode closes a window).
-	*/
+	bool moving = true;
+
 	while(device->run())
 	{
-		/*
-		Anything can be drawn between a beginScene() and an endScene()
-		call. The beginScene() call clears the screen with a color and
-		the depth buffer, if desired. Then we let the Scene Manager and
-		the GUI Environment draw their content. With the endScene()
-		call everything is presented on the screen.
-		*/
-		driver->beginScene(true, true, SColor(255,100,101,140));
-		
-		if (InRange(Player, shark)) {
-			//font->draw(L"In range", );
-			//font->draw(stringw(InRange(cube, cube2)).c_str(), rect<s32>(12, 80, 100, 100), SColor(1, 255, 255, 255));
-			
 
-			// WATCH OUT!! This causes the cube to be added to the array 60 times per second!
-			// Add this functionality later, if necessary. 
-			//objects.push_back(cube);
-			//font->draw(stringw(length).c_str(), rect<s32>(12, 80, 100, 100), SColor(1, 255, 255, 255));
+		driver->beginScene(true, true, SColor(255,100,101,140));
+		shark->setPosition(shark->getPosition() + vector3df(0, 0, -0.05));
+		
+		if (Col(Player, win, 10)) {
+			Player->setPosition(Player->getPosition() + vector3df(0, 0, 0));
+			font->draw(L"Congrats You win xDDDDD", rect<s32>(30, 100, 30, 100), SColor(255, 255, 255, 255));
+		}
+		else
+		{
+			Player->setPosition(Player->getPosition() + vector3df(0, 0, 0.05));
 		}
 
 		// collision code.
 		if (Col(Player, shark, 10)) {
+			// push player out of the way. 
 			Player->setPosition(Player->getPosition() + vector3df(rand() % 1 - 1,0,0));
 		}
-		else { Player->setPosition(Player->getPosition() + vector3df(0, 0, 0.05)); }
-		
+
 		if (Col(shark, Player, 10)) {
 			shark->setPosition(shark->getPosition() + vector3df(0, 0, 0));
 		}
-		else { shark->setPosition(shark->getPosition() + vector3df(0, 0, -0.05)); }
 		
 		smgr->drawAll();
 		guienv->drawAll();
 
 		driver->endScene();
 	}
-
-	/*
-	After we are done with the render loop, we have to delete the Irrlicht
-	Device created before with createDevice(). In the Irrlicht Engine, you
-	have to delete all objects you created with a method or function which
-	starts with 'create'. The object is simply deleted by calling ->drop().
-	See the documentation at irr::IReferenceCounted::drop() for more
-	information.
-	*/
 	device->drop();
 
 	return 0;
 }
-
-/*
-That's it. Compile and run.
-**/
 
