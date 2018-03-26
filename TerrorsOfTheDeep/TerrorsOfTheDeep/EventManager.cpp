a7e8a52c4fcd66c0a3584fa61a9732baa34e14cb
Simone Bakker <simonebakker94@gmail.com>
2018-03-26 15:17:46 +0200

    Added: Mouse events in Event receiver
    Event manager now also tracks mouse position and mouse buttons. Can be used same way as key input

--- TerrorsOfTheDeep/TerrorsOfTheDeep/EventManager.cpp
+++ TerrorsOfTheDeep/TerrorsOfTheDeep/EventManager.cpp
@@ -20,8 +20,34 @@ EventManager::EventManager()
 bool EventManager::OnEvent(const SEvent& event)
 {
 	// Remember whether each key is down or up
-	if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+	if (event.EventType == irr::EET_KEY_INPUT_EVENT) 
+	{
 		KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+	}
+
+	// Remember the mouse state
+	if (event.EventType == irr::EET_MOUSE_INPUT_EVENT)
+	{
+		switch (event.MouseInput.Event)
+		{
+		case EMIE_LMOUSE_PRESSED_DOWN:
+			MouseState.LeftButtonDown = true;
+			break;
+
+		case EMIE_LMOUSE_LEFT_UP:
+			MouseState.LeftButtonDown = false;
+			break;
+
+		case EMIE_MOUSE_MOVED:
+			MouseState.Position.X = event.MouseInput.X;
+			MouseState.Position.Y = event.MouseInput.Y;
+			break;
+
+		default:
+			// We won't use the wheel
+			break;
+		}
+	}
 
 	return false;
 }
@@ -32,8 +58,18 @@ bool EventManager::IsKeyDown(EKEY_CODE keyCode) const
 	return KeyIsDown[keyCode];
 }
 
+const EventManager::SMouseState & EventManager::GetMouseState(void) const
+{
+	return MouseState;
+}
+
 // To handle events:
 //
-// if (gameManager.receiver.IsKeyDown(irr::KEY_KEY_P)) {
-//	*code you want to happen*
-//}
+// Check if key is down:
+// if (gameManager.receiver.IsKeyDown(irr::KEY_KEY_P))
+//
+// Check if mouse button is down:
+// if (gameManager.receiver.GetMouseState().LeftButtonDown)
+//
+// Get mouse position:
+// gameManager.receiver.GetMouseState().Position.X
--- TerrorsOfTheDeep/TerrorsOfTheDeep/EventManager.h
+++ TerrorsOfTheDeep/TerrorsOfTheDeep/EventManager.h
@@ -16,9 +16,19 @@ private:
 public:
 	EventManager();
 
+	// We'll create a struct to record info on the mouse state
+	struct SMouseState
+	{
+		core::position2di Position;
+		bool LeftButtonDown;
+		SMouseState() : LeftButtonDown(false) { }
+	} MouseState;
+
 	// This is the one method that we have to implement
 	virtual bool OnEvent(const SEvent& event);
 
 	// This is used to check whether a key is being held down
 	virtual bool IsKeyDown(EKEY_CODE keyCode) const;
+
+	const SMouseState & GetMouseState(void) const;
 };
