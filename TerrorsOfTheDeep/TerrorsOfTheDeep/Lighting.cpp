787da843907c24b0b9904d3fd1e150518eced04e
Martin van der Wolf <martin.van.der.wolf.van.der.wolf@hva.nl>
2018-05-07 15:11:20 +0200

    Added: doxygen comments

--- TerrorsOfTheDeep/TerrorsOfTheDeep/Lighting.cpp
+++ TerrorsOfTheDeep/TerrorsOfTheDeep/Lighting.cpp
@@ -3,18 +3,19 @@
 using namespace irr;
 using namespace scene;
 
-/*Example use:
-For ambient scene light:
-Lighting(smgr).SetSceneLight(irr::video::SColorf(r,g,b,a));
-
-All CreateLight functions have two extra arguments after color and position, namely castShadow and parentNode. 
-These default to false and 0.
-
-ILightSceneNode* light1 = Lighting(smgr).CreatePointLight(irr::video::SColorf(r,g,b,a), irr::core::vector3df(0, 30, -40));
-or
-ILightSceneNode* light1 = Lighting(smgr).CreatePointLight(irr::video::SColorf(r,g,b,a), core::vector3df(0, 30, -40), true, 5);
-
-Directional and Spotlight also require a vector3 dir for rotation
+/** 
+*Example use:
+*For ambient scene light:
+*Lighting(smgr).SetSceneLight(irr::video::SColorf(r,g,b,a));
+*
+*All CreateLight functions have two extra arguments after color and position, namely castShadow and parentNode. 
+*These default to false and 0.
+*
+*ILightSceneNode* light1 = Lighting(smgr).CreatePointLight(irr::video::SColorf(r,g,b,a), irr::core::vector3df(0, 30, -40));
+*or
+*ILightSceneNode* light1 = Lighting(smgr).CreatePointLight(irr::video::SColorf(r,g,b,a), core::vector3df(0, 30, -40), true, 5);
+*
+*Directional and Spotlight also require a vector3 dir for rotation
 */
 
 
@@ -24,13 +25,13 @@ Lighting::Lighting(ISceneManager* tempSmgr)
 	smgr = tempSmgr;
 }
 
-// Updates the ambient scene light
+/** Changes the ambient scene light */
 void Lighting::SetSceneLight(irr::video::SColorf color)
 {
 	smgr->setAmbientLight(color);
 }
 
-// Creates a point light e.g. a torch
+/** Creates a point light e.g. a torch */
 ILightSceneNode* Lighting::CreatePointLight(irr::video::SColorf color, irr::core::vector3df pos, float radius, bool castShadow, ISceneNode* parentNode)
 {
 	ILightSceneNode* light = smgr->addLightSceneNode(parentNode, pos, color);
@@ -43,7 +44,7 @@ ILightSceneNode* Lighting::CreatePointLight(irr::video::SColorf color, irr::core
 	return light;
 }
 
-// Creates a directional light e.g. the sun
+/** Creates a directional light e.g. the sun */
 ILightSceneNode* Lighting::CreateDirectionalLight(irr::video::SColorf color, irr::core::vector3df pos, irr::core::vector3df dir, float radius, bool castShadow, ISceneNode* parentNode)
 {
 	ILightSceneNode* light = smgr->addLightSceneNode(parentNode, pos, color);
@@ -57,7 +58,7 @@ ILightSceneNode* Lighting::CreateDirectionalLight(irr::video::SColorf color, irr
 	return light;
 }
 
-// Creates a spot light e.g. a flashlight
+/** Creates a spot light e.g. a flashlight */
 ILightSceneNode* Lighting::CreateSpotLight(irr::video::SColorf color, irr::core::vector3df pos, irr::core::vector3df dir, float radius, bool castShadow, ISceneNode* parentNode)
 {
 	
